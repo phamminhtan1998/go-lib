3a9747642b18368d1092446863d89c27eea0761a
tanpm1 <tanpm1@mbbank.com.vn>
2023-01-12 15:03:26 +0700

    Add log

--- go-lib.go
+++ go-lib.go
@@ -1,11 +1,17 @@
 package lib
 
+import (
+	"fmt"
+)
+
 // Returns the sum of two numbers
 func Add(a int, b int) int {
+	fmt.Println("add data")
 	return a + b
 }
 
 // Returns the difference between two numbers
 func Subtract(a int, b int) int {
+	fmt.Println("substract data")
 	return a - b
 }
